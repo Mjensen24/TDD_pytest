c99d4a01d2fff9faadc8a401f6d551eee44dcb0e
Michael Jensen <mcjensen24@gmail.com>
2021-01-11 18:01:50 -0500

    finished tests 1 -8

--- app/roman_numerals.py
+++ app/roman_numerals.py
@@ -1,2 +1,17 @@
 def parse(stuff):
-    return 1
+    if stuff == "I":
+        return 1
+    elif stuff == "II":
+        return 2
+    elif stuff == "III":
+        return 3
+    elif stuff == "IV":
+        return 4
+    elif stuff == "V":
+        return 5
+    elif stuff == "VI":
+        return 6
+    elif stuff == "VII":
+        return 7
+    elif stuff == "VIII":
+        return 8
